9766bf3a1c8f1a9d5cc8a5e613d0df341ef32888
Azucena Sanchez <azuusanchez12@gmail.com>
2022-08-15 23:21:49 -0300

    libreria swal

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const contrasenia = document.querySelector("#contrasenia");
 const formularioIniciar = document.querySelector ("#form-iniciar");
 const ingresar = document.querySelector ("#submitIngresar");
 const registrarse = document.querySelector ("#submitRegistrarse");
+const newRegister = document.querySelector ("#registrarse");
+const formularioRegistrarse = document.querySelector( "#form-registrarse");
+const nombreYApellido = document.querySelector( "#nombreYApellido");
+const emailRegistro= document.querySelector( "#inputEmail");
+const usuarioRegistro = document.querySelector( "#inputUsuario");
+const contraRegistro = document.querySelector("#inputContrasenia");
+const inputRegistrarse = document.querySelector("#inputRegistrarse")
 
 
-let usuarios =[{
+
+
+
+
+
+
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) ||[{
     user: "azu.sanchez",
     contra: "miajazmin"
 }]
@@ -17,13 +30,13 @@ function usuarioIngresado () {
     let usuarioIngresado = usuarios.find(userU => userU.user === usuario.value)
 
     if (usuarioIngresado == undefined) {
-        Swal.fire(
+        Swal.fire (
             'No hay coinicidencia de Usuarios',
             'Registrese',
             
         )
     } else if (usuarioIngresado.contra !== contrasenia.value) {
-        Swal.fire(
+        Swal.fire (
             'Contraseña incorrecta',
             'error',
         )
@@ -31,9 +44,40 @@ function usuarioIngresado () {
         window.location.href = "turnos.html"
     }
 }
+console.log (usuarioIngresado)
 
-ingresar.onsubmit = (e) =>{
+formularioIniciar.onsubmit = (e) =>{
     e.preventDefault(),
     usuarioIngresado()
 }
+
+
+
+class Clientes {
+    constructor(nombreyApellido,email , usuario, contrasenia) {
+        this.nombreyApellido= nombreyApellido,
+        this.email = email,
+        this.usuario= usuario,
+        this.contrasenia= contrasenia
+    }
+}
+let nuevoRegistroDeUsuario = () =>{
+    const usuariosNuevos = new Clientes(nombreYApellido.value, emailRegistro.value, usuarioRegistro.value, contraRegistro.value)
+    usuarios.push(usuariosNuevos)
+
+}
+
+registrarse.onsubmit = (e) =>{
+    e.preventDefault()
+    registrarseId.style.display = "flex"
+    console.log (registrarse)
+    
+    formularioRegistrarse.onsubmit = (e) => {
+        e.preventDefault()
+    }
+
+}
+
+
+
 
